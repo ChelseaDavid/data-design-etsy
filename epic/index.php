642691f2cf05f31fcf577cfd97187b60958a4196
Chelsea David <cryan17@cnm.edu>
2017-07-13 20:39:26 -0600

    created basic html with image

---
+++ epic/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Data-Design-Etsy</title>
+	</head>
+	<body>
+		<h1>Persona:</h1>
+		<p><strong>Name:</strong> Meghan Jones</p>
+
+		<!--Added user image -->
+		<img src="images/etsy.jpg" alt="etsy">
+
+		<p><strong>Age:</strong>28</p>
+		<p><strong>Operating Platforms:</strong>Spends most of her time at home so prefers to operate on Windows 10 but occasionally uses tablet or smartphone when on the go or completing transactions</p>
+		<p><strong>User:</strong>Meghan is a college graduate who majored in fine art. She recently had a baby, and found lots of time while at home to begin pursuing her love for creating masterpieces.</p>
+		<p>Meghan has been working on her love of slupting and pottery work for about a year now and has come to find she has quite a few items in her collection. Upon hearing feedback from friends and relatives on what a great job she had done, she considered if others may feel the same.</p>
+
+		<h2>Needs:</h2>
+		<ul>
+			<li>to connect with like users who are in the market to purchase he masterpieces</li>
+			<li>to share pictures with users</li>
+			<li>to establish a strong profile to appeal to future and or returning customers</li>
+		</ul>
+
+		<h2>Frustrations</h2>
+		<ul>
+			<li></li>
+		</ul>
+	</body>
+	</html>
